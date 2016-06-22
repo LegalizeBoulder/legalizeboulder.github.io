8d14f5da41ea469c3494584ba9d9f157571f9a53
Gregor Robinson <gregor@fiatflux.co.uk>
2016-06-22 11:38:25 -0600

    Make HTML fragments use a sensible extension

--- generate-html.sh
+++ generate-html.sh
@@ -1,9 +1,9 @@
 #!/bin/env sh
 
 function generate_html {
-    cat frontmatter.htmlfragment >$1.html
-    cat $1.htmlfragment >>$1.html
-    cat backmatter.htmlfragment >>$1.html
+    cat frontmatter.fragment.html >$1.html
+    cat $1.fragment.html >>$1.html
+    cat backmatter.fragment.html >>$1.html
 }
 
 generate_html index
