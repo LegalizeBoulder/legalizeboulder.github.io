e976c6dccda5f89e88b4c83a78350a6dfc270326
Gregor Robinson <gregor@fiatflux.co.uk>
2016-06-22 11:28:44 -0600

    Improving workflow

---
+++ generate-html.sh
@@ -0,0 +1,10 @@
+#!/bin/env sh
+
+function generate_html {
+    cat frontmatter.htmlfragment >$1.html
+    cat $1.htmlfragment >>$1.html
+    cat backmatter.htmlfragment >$1.html
+}
+
+generate_html index
+generate_html newsletter
